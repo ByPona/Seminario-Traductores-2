38384f65a62ea5d941ece755855cf9df1a9143bc
Fernando Castro <84426610+ByPona@users.noreply.github.com>
2022-01-23 20:14:13 -0600

    Update lexico.py

--- Mini Generador Lexico/lexico.py
+++ Mini Generador Lexico/lexico.py
@@ -7,10 +7,8 @@ class Lexico(object):
         contador = 0
         cadena_resultado = ""
         entero_resultado = ""
-        simbolo_resultado = ""
         self.cadena = self.cadena + " "
         longitud = len(self.cadena)
-        longitud = longitud
 
         while contador<longitud:
             if self.cadena[contador].isalpha() == True:
@@ -26,19 +24,11 @@ class Lexico(object):
                 while self.cadena[contador]!=" ":
                     entero_resultado = entero_resultado + self.cadena[contador]
                     contador=contador+1
-                print(f"{entero_resultado:<20}{'Numero':>8}")
-
-            else:
-                simbolo_resultado = self.cadena[contador]
-                print(f"{simbolo_resultado:<20}{'Simbolo':>9}")
-                contador=contador+1
+                print(f"{entero_resultado:<20}{'Real':>6}")
 
             cadena_resultado = ""
             entero_resultado = ""
-            simbolo_resultado = ""
             if self.cadena[contador]==" ":
                 contador=contador+1
 
-            #print(contador)
-            #print(longitud)
         return ""
